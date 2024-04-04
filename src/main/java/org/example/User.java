8bdeafb53f5865400e4ab5802cabaf7f46fcf5d9
mr.detafti <dehghani735@gmail.com>
2024-04-04 19:34:20 +0330

    memento pattern added.

---
+++ src/main/java/org/example/User.java
@@ -0,0 +1,6 @@
+package org.example;
+
+public class User {
+
+    public String name;
+}
---
+++ src/main/java/org/example/state/ToolType.java
@@ -0,0 +1,7 @@
+package org.example.state;
+
+public enum ToolType {
+    SELECTION,
+    BRUSH,
+    ERASER
+}
