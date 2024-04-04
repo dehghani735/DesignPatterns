a63d427f9ca73cd9ba9e37d852571d4f9716952a
mr.detafti <dehghani735@gmail.com>
2024-04-04 23:15:17 +0330

    iterator benefit added.

--- src/main/java/org/example/iterator/BrowseHistory.java
+++ src/main/java/org/example/iterator/BrowseHistory.java
@@ -1,22 +1,25 @@
 package org.example.iterator;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class BrowseHistory {
 
-    private List<String> urls = new ArrayList<>(); // todo
+    private String[] urls = new String[10]; // todo
+    private int count;
 
     public void push(String url) {
-        urls.add(url);
+//        urls.add(url);
+        urls[count] = url;
+        count++;
     }
 
     public String pop() {
-        var lastIndex = urls.size() - 1;
-        var lastUrl = urls.get(lastIndex);
-        urls.remove(lastUrl);
+//        var lastIndex = urls.size() - 1;
+//        var lastUrl = urls.get(lastIndex);
+//        urls.remove(lastUrl);
+//
+//        return lastUrl;
 
-        return lastUrl;
+        count--;
+        return urls[count];
     }
 
     // we don't need this. because we don't want to expose it to others
@@ -25,25 +28,51 @@ public class BrowseHistory {
 //    }
 
     public Iterator createIterator() {
-        return new ListIterator(this);
+//        return new ListIterator(this);
+        return new ArrayIterator(this);
     }
 
-    public class ListIterator implements Iterator<String> {
+//    public class ListIterator implements Iterator<String> {
+//        private BrowseHistory history;
+//        private int index;
+//
+//        public ListIterator(BrowseHistory history) {
+//            this.history = history;
+//        }
+//
+//        @Override
+//        public boolean hasNext() {
+//            return (index < history.urls.size()); // I have access to urls and that is the reason I defined ListIterator inside BrowseHistory class
+//        }
+//
+//        @Override
+//        public String current() {
+//            return history.urls.get(index);
+//        }
+//
+//        @Override
+//        public void next() {
+//            index++;
+//        }
+//    }
+
+    public class ArrayIterator implements Iterator<String> {
+
         private BrowseHistory history;
         private int index;
 
-        public ListIterator(BrowseHistory history) {
+        public ArrayIterator(BrowseHistory history) {
             this.history = history;
         }
 
         @Override
         public boolean hasNext() {
-            return (index < history.urls.size()); // I have access to urls and that is the reason I defined ListIterator inside BrowseHistory class
+            return (index < history.count);
         }
 
         @Override
         public String current() {
-            return history.urls.get(index);
+            return history.urls[index];
         }
 
         @Override
