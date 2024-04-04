ba4233ff0b51349f5034f714ac1de95068f7d4d9
mr.detafti <dehghani735@gmail.com>
2024-04-04 23:56:37 +0330

    polished.

--- src/main/java/org/example/strategy/Main.java
+++ src/main/java/org/example/strategy/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
 //        var imageStorage = new ImageStorage(new JpegCompressor(), new BlackAndWhiteFilter());
         var imageStorage = new ImageStorage(new PngCompressor(), new BlackAndWhiteFilter());
-        imageStorage.store("a");
+        imageStorage.store("a"); // todo we can add parameters here. so we can use different compressors using
+        // one object of imageStorage
     }
 }
