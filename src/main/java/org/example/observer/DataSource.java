591db9a966cc12eee67fbbd00af188fb90181043
mr.detafti <dehghani735@gmail.com>
2024-04-05 11:54:20 +0330

    observer problem added.

---
+++ src/main/java/org/example/observer/DataSource.java
@@ -0,0 +1,14 @@
+package org.example.observer;
+
+public class DataSource {
+
+    private int value;
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+}
