2645f69b88bdbc460f49f228b3872b666a2e1cee
mr.detafti <dehghani735@gmail.com>
2024-04-05 16:24:01 +0330

    observer solution added with pull style.

--- src/main/java/org/example/observer/Chart.java
+++ src/main/java/org/example/observer/Chart.java
@@ -1,8 +1,14 @@
 package org.example.observer;
 
 public class Chart implements Observer {
+    private DataSource dataSource;
+
+    public Chart(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Override
     public void update() {
-        System.out.println("Chart got updated");
+        System.out.println("Chart got updated: " + dataSource.getValue());
     }
 }
--- src/main/java/org/example/observer/SpreadSheet.java
+++ src/main/java/org/example/observer/SpreadSheet.java
@@ -1,8 +1,15 @@
 package org.example.observer;
 
 public class SpreadSheet implements Observer {
+    private DataSource dataSource;
+
+    public SpreadSheet(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+
     @Override
     public void update() {
-        System.out.println("Spreadsheet got notified.");
+        System.out.println("Spreadsheet got notified: " + dataSource.getValue());
     }
 }
