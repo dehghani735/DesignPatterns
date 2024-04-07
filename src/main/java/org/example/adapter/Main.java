3cab4a33c8b3f400767a1012f6e031686b41dd01
mr.detafti <dehghani735@gmail.com>
2024-04-07 22:40:15 +0330

    Adapter solution added.

--- src/main/java/org/example/adapter/Main.java
+++ src/main/java/org/example/adapter/Main.java
@@ -6,6 +6,6 @@ public class Main {
     public static void main(String[] args) {
         var imageView = new ImageView(new Image());
 //        imageView.apply(new VividFilter());
-        imageView.apply(new Caramel());
+        imageView.apply(new CaramelFilter(new Caramel()));
     }
 }
