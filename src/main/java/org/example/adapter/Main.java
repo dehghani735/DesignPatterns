bf6e2993ae7025dc7e8b550dd62175702df03491
mr.detafti <dehghani735@gmail.com>
2024-04-07 22:03:21 +0330

    Adapter problem added.

---
+++ src/main/java/org/example/adapter/Main.java
@@ -0,0 +1,11 @@
+package org.example.adapter;
+
+import org.example.adapter.avaFilter.Caramel;
+
+public class Main {
+    public static void main(String[] args) {
+        var imageView = new ImageView(new Image());
+//        imageView.apply(new VividFilter());
+        imageView.apply(new Caramel());
+    }
+}
---
+++ src/main/java/org/example/adapter/avaFilter/Caramel.java
@@ -0,0 +1,14 @@
+package org.example.adapter.avaFilter;
+
+import org.example.adapter.Image;
+
+// This is a filter implementation in a third-party library
+public class Caramel {
+    public void init() {
+
+    }
+
+    public void render(Image image) {
+        System.out.println("Applying Caramel Filter");
+    }
+}
