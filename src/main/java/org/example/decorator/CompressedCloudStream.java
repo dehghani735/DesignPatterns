c3ec63d4bdb91c2ea0fc58b27a23462006869fda
mr.detafti <dehghani735@gmail.com>
2024-04-08 00:30:27 +0330

    Decorator solution added.

--- src/main/java/org/example/decorator/CompressedCloudStream.java
+++ src/main/java/org/example/decorator/CompressedCloudStream.java
@@ -1,10 +1,16 @@
 package org.example.decorator;
 
-public class CompressedCloudStream extends CloudStream {
+public class CompressedCloudStream implements Stream {
+    private Stream stream; // field of type Stream is important
+
+    public CompressedCloudStream(Stream stream) {
+        this.stream = stream;
+    }
+
     @Override
     public void write(String data) {
         var compressed = compress(data);
-        super.write(compressed);
+        stream.write(compressed);
     }
 
     private String compress(String data) {
--- src/main/java/org/example/decorator/Main.java
+++ src/main/java/org/example/decorator/Main.java
@@ -2,7 +2,16 @@ package org.example.decorator;
 
 public class Main {
     public static void main(String[] args) {
-        var cloudStream = new CloudStream();
-        cloudStream.write("Here's some data");
+//        var cloudStream = new CloudStream();
+//        cloudStream.write("Here's some data");
+
+        storeCreditCard(new CloudStream());
+        storeCreditCard(new EncryptedCloudStream(new CloudStream()));
+        // we are wrapping or decorating this cloud stream with an encrypted cloud stream
+    }
+
+    // this method is talking to an interface.
+    public static void storeCreditCard(Stream stream) {
+        stream.write("1234-1234-1234-1234");
     }
 }
