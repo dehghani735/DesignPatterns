8ae6eaa1a9ff6e6a1baf154846317f52df86f996
mr.detafti <dehghani735@gmail.com>
2024-04-08 23:11:43 +0330

    Flyweight problem added.

---
+++ src/main/java/org/example/flyweight/Main.java
@@ -0,0 +1,9 @@
+package org.example.flyweight;
+
+public class Main {
+    public static void main(String[] args) {
+        var service = new PointService();
+        for (var point: service.getPoints())
+            point.draw();
+    }
+}
