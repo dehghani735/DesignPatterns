95c0f3e8e12ee51d62a1fb9b7f051892df057341
mr.detafti <dehghani735@gmail.com>
2024-04-08 22:08:32 +0330

    Facade problem added.

---
+++ src/main/java/org/example/facade/Main.java
@@ -0,0 +1,12 @@
+package org.example.facade;
+
+public class Main {
+    public static void main(String[] args) {
+        var server = new NotificationServer();
+        var connection = server.connect("ip");
+        var authToken = server.authenticate("appId", "key");
+        var message = new Message("Hello World");
+        server.send(authToken, message, "target");
+        connection.disconnect();
+    }
+}
