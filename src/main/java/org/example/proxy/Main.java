52d231897190d8fc10f8e69a7422f1085e23bcfc
mr.detafti <dehghani735@gmail.com>
2024-04-10 12:06:10 +0330

    Proxy problem added.

---
+++ src/main/java/org/example/proxy/Main.java
@@ -0,0 +1,13 @@
+package org.example.proxy;
+
+public class Main {
+    public static void main(String[] args) {
+        var library = new Library();
+        String[] fileNames = {"a", "b", "c"};
+        for (var fileName : fileNames) {
+            library.add(new Ebook(fileName));
+        }
+
+        library.openEbook("a");
+    }
+}
