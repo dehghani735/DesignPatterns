682546f4dfb9f20a7324f1fd1eaa7da768754197
mr.detafti <dehghani735@gmail.com>
2024-04-10 23:05:55 +0330

    Prototype problem added.

---
+++ src/main/java/org/example/prototype/ContextMenu.java
@@ -0,0 +1,12 @@
+package org.example.prototype;
+
+public class ContextMenu {
+    public void duplicate(Component component) {
+        if (component instanceof Circle) {
+            Circle source = (Circle) component;
+            Circle target = new Circle();
+            target.setRadius(source.getRadius());
+            // Add target to our document
+        }
+    }
+}
