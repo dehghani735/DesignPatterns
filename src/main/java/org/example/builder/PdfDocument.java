d558152f3151ba9d2ed67fbe7751837da4cc695d
mr.detafti <dehghani735@gmail.com>
2024-04-11 17:28:40 +0330

    builder problem added.

---
+++ src/main/java/org/example/builder/PdfDocument.java
@@ -0,0 +1,7 @@
+package org.example.builder;
+
+public class PdfDocument {
+    public void addPage(String text) {
+        System.out.println("Adding a page to PDF");
+    }
+}
---
+++ src/main/java/org/example/builder/PresentationFormat.java
@@ -0,0 +1,8 @@
+package org.example.builder;
+
+public enum PresentationFormat {
+    PDF,
+    IMAGE,
+    POWERPOINT,
+    MOVIE
+}
